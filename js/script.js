a5e97fcb15c46369e6e08d1988f7f52d8eb36634
developerXdecoded <34496647+developerXdecoded@users.noreply.github.com>
2018-02-11 07:44:17 -0600

    nav bar appear feature

--- js/script.js
+++ js/script.js
@@ -1,23 +1,36 @@
-console.log("it works bro !!");
+console.log("it works !!");
 
 
 // variables
-var nav_bar = document.getElementById("header"),
-body = document.body,
-docElem = document.documentElement,
-offset = 100,
-scrollPos,
-docHeight;
+var nav_bar = document.getElementsByTagName("HEADER")[0],
+  body = document.body,
+  docElem = document.documentElement,
+  offset = 100,
+  scrollPos,
+  docHeight;
 
 // calucate doc height
 
 docHeight = Math.max(body.scrollHeight, body.offsetHeight, docElem.clientHeight, docElem.scrollHeight, docElem.offsetHeight);
 if (docHeight != 'undefined') {
-  offset = docHeight / 4;
+  offset = docHeight / 16;
 }
 
 // scroll event listener
 
 window.addEventListener("scroll", function(event) {
-    scrollPos = body.scroll
-})
+    scrollPos = body.scrollTop || docElem.scrollTop;
+
+    nav_bar.className = ( scrollPos > offset ) ? "visible" : "";
+
+});
+
+
+// var banner = document.querySelector('.banner--title');
+//
+// banner.textContent = 'KEVIN';
+// var nav_bar_show = document.getElementsByTagName("HEADER")[0];
+// var att = document.createAttribute("class");
+//
+// att.value = "visible";
+// nav_bar_show.setAttributeNode(att);
